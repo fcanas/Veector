c1d2f5f1c99700ba2009c2514504faffcc40dbe7
Fabian Canas <fcanas@gmail.com>
2014-12-04 21:40:56 -0500

    Initial Commit

---
+++ Veector/VectorFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  CGVector.swift
+//  Veector
+//
+//  Created by Fabian Canas on 12/4/14.
+//  Copyright (c) 2014 Fabián Cañas. All rights reserved.
+//
+
+import Foundation
---
+++ Veector/Veector.h
@@ -0,0 +1,19 @@
+//
+//  Veector.h
+//  Veector
+//
+//  Created by Fabian Canas on 12/4/14.
+//  Copyright (c) 2014 Fabián Cañas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Veector.
+FOUNDATION_EXPORT double VeectorVersionNumber;
+
+//! Project version string for Veector.
+FOUNDATION_EXPORT const unsigned char VeectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Veector/PublicHeader.h>
+
+
